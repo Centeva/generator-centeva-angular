0abf58e43beee1839c4794fe7c4a608c1c04f9e4
Travis Marble <travis.marble@centeva.com>
2015-10-20 10:53:14 -0600

    add as vm to .js files.

--- generators/modal/templates/modal.js
+++ generators/modal/templates/modal.js
@@ -1,6 +1,11 @@
 angular.module('<%= appname %>')
-    .controller('<%= ctrlname %>', function ($scope) {
-
+    .controller('<%= ctrlname %>', function ($scope, $modalInstance) {
+        var vm = this;
+        
+        // vm.save = function(){
+        //     $modalInstance.close(result);
+        // }
+        
         var destroy = $scope.$on("$destroy", function () {
             //Cleanup anything that would persist beyond destruction, including $on/$watch
             destroy();
